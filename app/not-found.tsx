b443a50ca9fc1d5c2dc4ff87e5069a212d06cbbe
WisW <78122384+winsrewu@users.noreply.github.com>
2025-01-13 19:17:38 +0800

    fix: use Link instead of a

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Page() {
     return (
         <div className="absolute m-auto inset-x-0 inset-y-0 w-1/2 h-1/2 text-gray-100 text-center text-xl">
             404 | Not Found <br /><br />
-            <a href="/" className="hover:text-white"> Click here to redirect to home page. </a> <br />
-            <a href="/" className="hover:text-white"> 点这里以重定向到主页面. </a>
+            <Link href="/" className="hover:text-white"> Click here to redirect to home page. </Link> <br />
+            <Link href="/" className="hover:text-white"> 点这里以重定向到主页面. </Link>
         </div>
     );
 }
